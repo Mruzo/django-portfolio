25782b0bf78efacfd125e74788d75c877a4eb872
Mr Uzo <chrisuzoewulu@gmail.com>
2020-04-24 21:04:16 -0400

    changed user permissions

--- projects/admin.py
+++ projects/admin.py
@@ -1,7 +1,73 @@
+from typing import Set
 from django.contrib import admin
 from .models import Project
+from django.contrib.auth.models import User
+from django.contrib.auth.admin import UserAdmin
+
+
+#unregister provided moded admin
+admin.site.unregister(User)
+
 
 # Register your models here.
 
-
 admin.site.register(Project)
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    readonly_fields = [
+        'date_joined',
+    ]
+
+    #custom action to mark multiple users as active
+    actions = [
+        'activate_users',
+    ]
+
+    def activate_users(self, request, queryset):
+        cnt = queryset.filter(is_active=False).update(is_active=True)
+        self.message_user(request, 'Activated {} users.'.format(cnt))
+    activate_users.short_description = 'Activate Users'
+
+    #To hide custom action from from users without change permission
+    def get_actions(self, request):
+        actions = super().get_actions(request)
+        if not request.user.has_perm('auth.change_user'):
+            del actions['activate_users']
+        return actions
+
+    def has_delete_permission(self, request, obj=None):
+        return False
+
+    def get_form(self, request, obj=None, **kwargs):
+        form = super().get_form(request, obj, **kwargs)
+        is_superuser = request.user.is_superuser
+        disabled_fields = set()
+
+        # Prevent superusers from granting superuser rights
+        if not is_superuser:
+            disabled_fields |= {
+                'username',
+                'is_superuser',
+                'user_permissions',
+            }
+
+        # Prevent non-superusers from editing their own permissions
+        if (
+            not is_superuser
+            and obj is not None
+            and obj == request.user
+        ):
+            disabled_fields |= {
+                'is_staff',
+                'is_superuser',
+                'groups',
+                'user_permissions',
+            }
+
+        for f in disabled_fields:
+            if f in form.base_fields:
+                form.base_fields[f].disabled = True
+
+        return form
